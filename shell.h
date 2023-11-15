3802c441c1d10a47bb9ad41df57ed4826c7a1841
Tauraim <tmuzembe@outlook.com>
2023-11-15 03:11:14 -0800

    working on builtin commands getline and adding the .h file

--- shell.h
+++ shell.h
@@ -3,16 +3,44 @@
 
 #include <stdio.h>
 #include <unistd.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
 
 char *empty_line(char *store, char **agv);
 int exit_shell(char **argv);
 char **_env(char **argv, char **env);
 int _stat(struct stat st, char **argv, char bin[100], char *tok, char *store);
+
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* for command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
+/* for convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/* 1 if using system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+extern char **environ;
+
 
 #endif
